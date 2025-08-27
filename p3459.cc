316d0b3e392e34cfb03c051d8a8c3d59dfbe7095
Ramon Gonzalez Fernandez (M1) <git@damastacoda.dev>
2025-08-27 12:43:49 -0400

    P3459: Flatten Grid for Cache + Clean Up

--- p3459.cc
+++ p3459.cc
@@ -1,9 +1,10 @@
-#include <ios>
-#include <utility>
 #pragma GCC optimize("O3", "unroll-loops")
 
 #include <algorithm>
+#include <ios>
 #include <iostream>
+#include <numeric>
+#include <utility>
 #include <vector>
 
 inline std::pair<int, int> operator+(const std::pair<int, int> &lhs,
@@ -11,40 +12,61 @@ inline std::pair<int, int> operator+(const std::pair<int, int> &lhs,
   return {lhs.first + rhs.first, lhs.second + rhs.second};
 }
 
+struct Grid {
+  int width;
+  int height;
+
+  std::vector<int> data;
+
+  Grid() {};
+
+  Grid(const std::vector<std::vector<int>> &grid)
+      : width(grid.size()), height(grid.front().size()) {
+    data.reserve(width * height);
+
+    data =
+        std::accumulate(grid.begin(), grid.end(), data,
+                        [](std::vector<int> acc, const std::vector<int> &vec) {
+                          acc.insert(acc.end(), vec.begin(), vec.end());
+                          return acc;
+                        });
+  }
+
+  int operator[](std::pair<int, int> pos) const {
+    return data[pos.first * height + pos.second];
+  }
+};
+
 class Solution {
   constexpr static std::pair<int, int> direction_lookup[]{
       {1, -1}, {-1, -1}, {-1, 1}, {1, 1}};
 
-  int grid_width;
-  int grid_height;
-
   std::vector<int> memoization_table;
+  Grid grid;
 
 public:
-  int lenOfVDiagonal(std::vector<std::vector<int>> &grid) {
+  int lenOfVDiagonal(std::vector<std::vector<int>> &grid_nested) {
+    grid = Grid(grid_nested);
 
     // Decouple stdio from iostreams.
     // Faster, but race condition occurs if you use both.
     std::ios::sync_with_stdio(false);
 
-    grid_width = grid.size();
-    grid_height = grid[0].size();
-
-    memoization_table.assign(grid_width * grid_height * 16, -1);
+    memoization_table.assign(grid.width * grid.height * 16, -1);
 
     int r = 0;
 
-    for (int x = 0; x < grid_width; x++) {
-      for (int y = 0; y < grid_height; y++) {
-        if (grid[x][y] == 1) {
-          std::pair<int, int> pos{x, y};
+    for (int x = 0; x < grid.width; x++) {
+      for (int y = 0; y < grid.height; y++) {
+        std::pair<int, int> pos{x, y};
+        if (grid[pos] == 1) {
 
           r = std::max({
               r,
-              recursive_pather(grid, 0, pos + direction_lookup[0], false) + 1,
-              recursive_pather(grid, 1, pos + direction_lookup[1], false) + 1,
-              recursive_pather(grid, 2, pos + direction_lookup[2], false) + 1,
-              recursive_pather(grid, 3, pos + direction_lookup[3], false) + 1,
+              recursive_pather(0, pos + direction_lookup[0], false) + 1,
+              recursive_pather(1, pos + direction_lookup[1], false) + 1,
+              recursive_pather(2, pos + direction_lookup[2], false) + 1,
+              recursive_pather(3, pos + direction_lookup[3], false) + 1,
           });
         }
       }
@@ -54,13 +76,12 @@ public:
   }
 
   // can move and turn
-  int recursive_pather(std::vector<std::vector<int>> &grid, int direction,
-                       std::pair<int, int> position, bool alternating_state,
-                       bool has_turned = false) {
+  int recursive_pather(int direction, std::pair<int, int> position,
+                       bool alternating_state, bool has_turned = false) {
     // Check bounds before memo since memo is flat vec
-    if (position.first < 0 || position.first >= grid_width ||
-        position.second < 0 || position.second >= grid_height ||
-        grid[position.first][position.second] != (alternating_state ? 0 : 2))
+    if (position.first < 0 || position.first >= grid.width ||
+        position.second < 0 || position.second >= grid.height ||
+        grid[position] != (alternating_state ? 0 : 2))
       return 0;
 
     // Memo table uses lower 2^4=16 bits for non-pos state:
@@ -68,19 +89,19 @@ public:
     // 1,2 - direction
     // 3 - turned
     int memoization_index = position.first * 16 +
-                            position.second * grid_width * 16 + has_turned * 8 +
+                            position.second * grid.width * 16 + has_turned * 8 +
                             direction * 2 + alternating_state;
     if (memoization_table[memoization_index] != -1)
       return memoization_table[memoization_index];
 
-    int out = 1 + recursive_pather(grid, direction,
-                                   position + direction_lookup[direction],
-                                   !alternating_state, has_turned);
+    int out =
+        1 + recursive_pather(direction, position + direction_lookup[direction],
+                             !alternating_state, has_turned);
 
     if (!has_turned) {
       auto new_direction = (direction + 1) % 4;
       out = std::max(
-          out, 1 + recursive_pather(grid, new_direction,
+          out, 1 + recursive_pather(new_direction,
                                     position + direction_lookup[new_direction],
                                     !alternating_state, true));
     }
