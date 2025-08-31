cb036731199c6edf4b0fab2a2097fa1fbfc935fe
Ramon Gonzalez Fernandez (M1) <git@damastacoda.dev>
2025-08-30 23:44:06 -0400

    P36: Speed Up

--- p36.cc
+++ p36.cc
@@ -1,13 +1,37 @@
+#include <span>
 #pragma GCC optimize("O3", "unroll-loops")
 #pragma GCC option("arch=native", "tune=native")
 #include <ios>
 auto _ = std::ios::sync_with_stdio(false);
 
+#include <cstddef>
 #include <vector>
+
+template <class T> struct Grid {
+  std::vector<T> data;
+  int height, width;
+
+  Grid(std::vector<std::vector<T>> &nums) {
+    height = nums.size();
+    width = nums.front().size();
+
+    nums.reserve(width * height);
+    for (size_t y = 0; y < height; y++) {
+      for (size_t x = 0; x < width; x++) {
+        data.push_back(nums[y][x]);
+      }
+    }
+  }
+
+  std::span<T> operator[](size_t y) {
+    return std::span{data.begin() + y * 9, 9};
+  }
+};
 
 class Solution {
 public:
-  bool isValidSudoku(std::vector<std::vector<char>> &board) {
+  bool isValidSudoku(std::vector<std::vector<char>> &b) {
+    Grid<char> board{b};
     bool has_number[9];
 
     for (int i = 0; i < 9; i++)
